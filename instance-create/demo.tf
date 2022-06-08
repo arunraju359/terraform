f3640478e60d36ab82a5f0d20b1502e27835e5ff
arunraju359 <arunraju359@gmail.com>
2022-06-08 23:08:05 +0530

    Commit

--- instance-create/demo.tf
+++ instance-create/demo.tf
@@ -5,7 +5,7 @@
 resource "aws_instance" "sample" {
   ami                     = "ami-0022f774911c1d690"
   instance_type           = "t2.micro"
-  security_group        = [aws_security_group.allow_ssh]
+  security_groups        = [aws_security_group.allow_ssh]
 
 
   tags = {
