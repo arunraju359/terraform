96ed574bd6e77b4d0e10ee9e08add4d25c75e797
arunraju359 <arunraju359@gmail.com>
2022-06-08 23:06:55 +0530

    Commit

--- instance-create/demo.tf
+++ instance-create/demo.tf
@@ -5,7 +5,7 @@
 resource "aws_instance" "sample" {
   ami                     = "ami-0022f774911c1d690"
   instance_type           = "t2.micro"
-  vpc_security_group_ids         = "aws_security_group.allow_ssh.id"
+  security_group_ids         = [aws_security_group.allow_ssh.id]
 
 
   tags = {
