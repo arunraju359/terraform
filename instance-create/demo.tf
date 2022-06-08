d146ee2daf56cc72af7a8670f6a18a70a0c4f880
arunraju359 <arunraju359@gmail.com>
2022-06-08 21:48:05 +0530

    Commit

--- instance-create/demo.tf
+++ instance-create/demo.tf
@@ -1,3 +1,7 @@
+// resource "aws_instance" "web" {
+//The aws_instance.web resource block defines an aws_instance resource named web to create an AWS EC2 instance.
+//The arguments inside the aws_instance.web resource block specify what type of resource to create.
+
 resource "aws_instance" "sample" {
   ami                     = "ami-0022f774911c1d690"
   instance_type           = "t2.micro"
