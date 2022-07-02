055a3991b5db9751fab1fb9090311a08d8f2cabd
arunraju359 <arunraju359@gmail.com>
2022-07-02 10:28:01 +0530

    Pushing to roles

--- pract-session/variable.tf
+++ pract-session/variable.tf
@@ -1,3 +1,12 @@
 output "sample"{
     value = "Hello world"
 }
+
+
+variable "list"{
+    default=["Chennai","Karnataka","Bangalore"]
+}
+
+output "sample-list"{
+    value= "The capital of TN is ${var.list[0]}"
+}
