eb5d418740fd5b1e26935d00d0e7ecc01d271e30
arunraju359 <arunraju359@gmail.com>
2022-07-03 18:57:22 +0530

    Pushing to roles

--- pract-session/provider.tf
+++ pract-session/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "mybucket-for-state"
-    key    = "dev/tform/"
+    key    = "dev/tform/terraform.tfstate"
     region = "us-east-1"
   }
 }
