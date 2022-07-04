47d47fd26c62136aa2d90f0ccf079119028f2e02
arunraju359 <arunraju359@gmail.com>
2022-07-04 15:28:04 +0530

    Pushing to roles

--- loops/ec2loops.tf
+++ loops/ec2loops.tf
@@ -1,3 +1,13 @@
+provider "aws"{
+    region = "us-east-1"
+}
+
+
+
+
+
+
+
 resource "aws_instance" "sample" {
   ami                     = "ami-0022f774911c1d690"
   instance_type           = "t2.micro"
