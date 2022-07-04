fb9dfb670a8569f574e18a1d48009d328ada8e2d
arunraju359 <arunraju359@gmail.com>
2022-07-04 15:27:11 +0530

    Pushing to roles

--- loops/ec2loops.tf
+++ loops/ec2loops.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "sample" {
   ami                     = "ami-0022f774911c1d690"
   instance_type           = "t2.micro"
-  vpc_security_group_ids       = [aws_security_group.allow_ssh.id]
+  
   key_name          = "aws-devops-learn-22"
   count = 2
 
