7aa104ff13312bfa2a018a7a205f26bdccb400d7
arunraju359 <arunraju359@gmail.com>
2022-08-01 20:59:14 +0530

    commit

--- all-in-one/ec2.tf
+++ all-in-one/ec2.tf
@@ -2,6 +2,10 @@ resource "aws_instance" "example" {
   ami           = "ami-08df646e18b182346"
   instance_type = "t2.micro"
   key_name = "newKeyMumbai"
+  tags = {
+    Name = "Test-Machine"
+  }
+
   security_groups = ["${aws_security_group.allow_rdp.name}"]
 
 }
