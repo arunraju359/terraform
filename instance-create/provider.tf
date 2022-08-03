274123fe756ea963ec9d21bde3a4c546860a79da
arunraju359 <arunraju359@gmail.com>
2022-08-03 08:08:21 +0530

    commit

--- instance-create/provider.tf
+++ instance-create/provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
     region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "my-bucket-for-tf-state"
+    key    = "dev/ec2/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
