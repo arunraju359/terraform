c74860bbe7a60316afdcd8c4c130929d4d47ea06
arunraju359 <arunraju359@gmail.com>
2022-08-14 15:40:57 +0530

    commit

--- terraform-vpc/subnet.tf
+++ terraform-vpc/subnet.tf
@@ -1,7 +1,8 @@
-resource "aws_default_subnet" "Public" {
-  availability_zone = "us-west-2a"
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "172.30.1.0/24"
 
   tags = {
-    Name = "Public-Subnet-A"
+    Name = "Public-A"
   }
 }
