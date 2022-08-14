dd57b977dac0cc102494868274090c7d707d9c25
arunraju359 <arunraju359@gmail.com>
2022-08-14 17:31:02 +0530

    commit

--- terraform-vpc/igw.tf
+++ terraform-vpc/igw.tf
@@ -1,9 +1,7 @@
-resource "aws_internet_gateway_attachment" "example" {
-  internet_gateway_id = aws_internet_gateway.example.id
-  vpc_id              = aws_vpc.main.id
-}
-resource "aws_vpc" "main" {
-  cidr_block = "172.32.0.0/16"
-}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
 
-resource "aws_internet_gateway" "example" {}
+  tags = {
+    Name = "main-gw"
+  }
+}
