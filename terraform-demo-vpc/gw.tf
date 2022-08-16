a408f71ee94d2f3ea4e43fddcd2fb90fcfc1f58c
arunraju359 <arunraju359@gmail.com>
2022-08-16 18:23:46 +0530

    commit

---
+++ terraform-demo-vpc/gw.tf
@@ -0,0 +1,25 @@
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main-demo.id
+
+  tags = {
+    Name = "${var.PROJECT_NAME}-${var.ENV}-igw"
+  }
+}
+
+resource "aws_eip" "eip" {
+  
+  vpc      = true
+}
+
+resource "aws_nat_gateway" "natgw" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public.*.id[0]
+
+  tags = {
+    Name = "${var.PROJECT_NAME}-${var.ENV}-natgw"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_eip.eip]
+}
