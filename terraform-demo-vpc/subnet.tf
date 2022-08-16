a8d17a2356dc2fae6af0477c45895119d52132aa
arunraju359 <arunraju359@gmail.com>
2022-08-16 10:46:34 +0530

    commit

--- terraform-demo-vpc/subnet.tf
+++ terraform-demo-vpc/subnet.tf
@@ -1,18 +1,21 @@
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main-demo.id
-  count = 
-  cidr_block = "10.0.1.0/24"
+  count      = length(var.PRIVATE_SUBNET_CIDR) 
+
+  cidr_block = element(var.PRIVATE_SUBNET_CIDR, count.index)
 
   tags = {
-    Name = "Main"
+    Name = "${var.PROJECT_NAME}-${var.ENV}-private-subnet-${count.index+1}"
   }
 }
 
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main-demo.id
-  cidr_block = "10.0.1.0/24"
+  count      = length(var.PUBLIC_SUBNET_CIDR) 
+
+  cidr_block = element(var.PUBLIC_SUBNET_CIDR, count.index)
 
   tags = {
-    Name = "Main"
+    Name = "${var.PROJECT_NAME}-${var.ENV}-public-subnet-${count.index+1}"
   }
 }
--- terraform-demo-vpc/vars.tf
+++ terraform-demo-vpc/vars.tf
@@ -1,5 +1,7 @@
 variable "VPC_CIDR" {}
 variable "PROJECT_NAME" {}
 variable "ENV" {}
+variable "PRIVATE_SUBNET_CIDR" {}
+variable "PUBLIC_SUBNET_CIDR" {}
 
   
