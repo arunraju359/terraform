85372a437e3d8dbe1dff15cd6926423c17baff68
arunraju359 <arunraju359@gmail.com>
2022-08-16 10:09:36 +0530

    commit

--- terraform-demo-vpc/config/dev-backend.tf
+++ terraform-demo-vpc/config/dev-backend.tf
@@ -1,8 +1,5 @@
-terraform {
-  backend "s3" {
-    bucket = "my-bucket-for-tf-state"
-    key    = "student/vpc/dev/terraform.tfstate"
-    region = "ap-south-1"
-    dynamodb_table = "terraform-lock"
-  }
-}
+bucket = "my-bucket-for-tf-state"
+key    = "student/vpc/dev/terraform.tfstate"
+region = "ap-south-1"
+dynamodb_table = "terraform-lock"
+ 
