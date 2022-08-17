a6465ede46b18c14e222f52c2590761fae8943b7
arunraju359 <arunraju359@gmail.com>
2022-08-17 05:54:20 +0530

    commit

--- terraform-demo-vpc/route.tf
+++ terraform-demo-vpc/route.tf
@@ -21,3 +21,17 @@ resource "aws_route_table" "private" {
     Name = "${var.PROJECT_NAME}-${var.ENV}-private-rt"
   }
 }
+
+
+resource "aws_route_table_association" "public" {
+  count      =  length(var.PUBLIC_SUBNET_CIDR) 
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_route_table_association" "private" {
+  count      =  length(var.PRIVATE_SUBNET_CIDR) 
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = aws_route_table.private.id
+}
